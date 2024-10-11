19a54f21ff0be1c5a6395cbcff2d246d23858d4c
KavinduSalinda <118047241+KavinduSalinda@users.noreply.github.com>
2024-10-11 07:23:40 +0530

    Refactor home page layout CSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
         <header>
           <img src={homeImage} alt="Home banner" className="w-full max-h-[90vh]"/>
           <div className="relative">
-            <div className="md:h-[10hv] lg:left-[5vw] md:bottom-[50px] bg-gradient-to-br from-cyan-400 to-green-400 lg:absolute w-[100vw] lg:w-[45%] z-10">
+            <div className="md:h-[10hv] lg:left-[60px] md:bottom-[50px] bg-gradient-to-br from-cyan-400 to-green-400 lg:absolute w-[100vw] lg:w-[45%] z-10">
               <div className="text-white text-5xl p-6">
                 <b>
                   We crush your competitors, goals, and sales records - without
